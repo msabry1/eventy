ae353da9859abd20eab55cd6ffa2b51050fd96d3
janagad <167188623+janagad@users.noreply.github.com>
2024-11-05 22:23:27 +0200

    added changes

---
+++ backend/eventy/src/main/java/com/eventy/repository/EventRepository.java
@@ -0,0 +1,10 @@
+package com.eventy.repository;
+
+import com.eventy.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+    //our custom queries
+}
