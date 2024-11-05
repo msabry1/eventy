1211672cf691293de84080e6974dbbaeef3caa07
janagad <167188623+janagad@users.noreply.github.com>
2024-11-05 21:01:13 +0200

    addingjpas

---
+++ backend/eventy/src/main/java/com/eventy/jparepos/UserRepository.java
@@ -0,0 +1,10 @@
+package com.eventy.jparepos;
+
+import com.eventy.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    //our custom queries
+}
