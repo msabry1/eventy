c4421ff697c1a91e1718105e5675d309acf33cb7
Michael Magdy <michaelmagdymagdy@gmail.com>
2024-12-14 03:31:45 +0200

    removed unused text

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import EventTicket from './components/ticket/EventTicket'
 function App() {
   return (
     <>
-      <h1 className='bg-slate-400'> COOL</h1>
       <EventTicket
         eventName="Cairokeee Concert"
         dateTime="2nd Dec 2024"
