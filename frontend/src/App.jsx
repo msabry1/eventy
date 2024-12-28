d1e8062ff1080567b3c873abeec9177b5721b8d4
Michael Magdy <michaelmagdymagdy@gmail.com>
2024-12-28 03:44:11 +0200

    first try

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,65 @@
-import { useState } from 'react'
-import SignUp from "./pages/sign-up/sign-up"
-import SignIn from "./pages/sign-in/sign-in"
-import ProfilePage from './pages/ProfilePage/profilePage'
-import TicketsPage from './pages/TicketsPage/TicketsPage'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import EventFeed from './pages/FeedPage/eventFeed'
-import EventPage from './pages/EventPage/eventPage'
-import CreateEventPage from './pages/CreateEvent/createEventPage'
+import { useContext } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { UserProvider, useUser } from "./context/UserContext";
+import SignUp from "./pages/sign-up/sign-up";
+import SignIn from "./pages/sign-in/sign-in";
+import ProfilePage from "./pages/ProfilePage/profilePage";
+import TicketsPage from "./pages/TicketsPage/TicketsPage";
+import EventFeed from "./pages/FeedPage/eventFeed";
+import EventPage from "./pages/EventPage/eventPage";
+import CreateEventPage from "./pages/CreateEvent/createEventPage";
 
 function App() {
-
+  const { isAuthenticated } = useUser(UserProvider); // Access authentication status
 
   return (
-    <>
-
-      <Router>
+    <Router>
       <Routes>
-      <Route path="" element={<ProfilePage/>} />
+        {/* Public Routes */}
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/signUp" element={<SignUp />} />
 
-      <Route path="/signIn" element={<SignIn/>} />
-      <Route path="/signUp" element={<SignUp/>} />
-      <Route path="/myProfile" element={<ProfilePage/>} />
-      <Route path="/myTickets" element={<TicketsPage/>} />
-      <Route path="/myFeed" element={<EventFeed/>} />
-      <Route path="/myProfile/createEvent" element={<CreateEventPage/>} />
-      <Route path="/eventPage" element={<EventPage/>} />
-
-
-
-
+        {/* Protected Routes */}
+        <Route
+          path="/"
+          element={
+            isAuthenticated ? <ProfilePage /> : <Navigate to="/signIn" />
+          }
+        />
+        <Route
+          path="/myProfile"
+          element={
+            isAuthenticated ? <ProfilePage /> : <Navigate to="/signIn" />
+          }
+        />
+        <Route
+          path="/myTickets"
+          element={
+            isAuthenticated ? <TicketsPage /> : <Navigate to="/signIn" />
+          }
+        />
+        <Route
+          path="/myFeed"
+          element={isAuthenticated ? <EventFeed /> : <Navigate to="/signIn" />}
+        />
+        <Route
+          path="/myProfile/createEvent"
+          element={
+            isAuthenticated ? <CreateEventPage /> : <Navigate to="/signIn" />
+          }
+        />
+        <Route
+          path="/eventPage"
+          element={isAuthenticated ? <EventPage /> : <Navigate to="/signIn" />}
+        />
       </Routes>
     </Router>
-
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/pages/sign-in/sign-in.jsx
+++ frontend/src/pages/sign-in/sign-in.jsx
@@ -1,56 +1,106 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from './SignIn.module.css'; // Import CSS Module
-import logo from '../../assets/logo.png';
-import googleLogo from '../../assets/google-logo.png';
-import { useNavigate } from 'react-router-dom';
-
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "./SignIn.module.css"; // Import CSS Module
+import logo from "../../assets/logo.png";
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { useLogin } from "../../hooks/useAuth";
+import { getAuthData, setAuthData } from "../../services/authAxios";
 
 function SignIn() {
-
+  const { login } = useLogin(); // Access login function from AuthContext
   const navigate = useNavigate();
-  
-const handleSumbit = () => {
-  navigate('/myFeed');
-};
-const handleSignUp = () =>{
-  navigate('/signUp')
-}
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState(""); // For error handling
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = formData.email;
+    const password = formData.password;
+    
+    const success = await login(email, password);
+    if (success) {
+        const { user } = getAuthData();
+        setAuthData({ user, isLoading: false });
+        navigate("/");
+    }
+  };
+
+  const handleSignUp = () => {
+    navigate("/signUp");
+  };
+
   return (
     <div className={styles.signInContainer}>
       <div className={styles.signInBox}>
         <Form.Label className={styles.titleSignIn}>Sign In</Form.Label>
-        <Button variant="primary" type="submit" className={styles.googleButton} >
-          <img src={googleLogo} alt="Google Logo" className={styles.googleLogo} />
-          Continue with Google
-        </Button>
-        <div className={styles.separator}>
-          <div className={styles.line}></div>
-          <span className={styles.separatorText}>or continue with Email</span>
-          <div className={styles.line}></div>
-        </div>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" className={styles.inputSignin} />
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              className={styles.inputSignin}
+              value={formData.email}
+              onChange={handleInputChange}
+              required
+            />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" className={styles.inputSignin} />
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Password"
+              className={styles.inputSignin}
+              value={formData.password}
+              onChange={handleInputChange}
+              required
+            />
           </Form.Group>
-          <Button variant="primary" type="submit" className={styles.submitSignIn} onClick={handleSumbit}>
+          {error && <p className={styles.errorText}>{error}</p>}{" "}
+          {/* Display error */}
+          <Button
+            variant="primary"
+            type="submit"
+            className={styles.submitSignIn}
+          >
             Submit
           </Button>
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Remember me" className={styles.checkboxSignIn} />
+            <Form.Check
+              type="checkbox"
+              label="Remember me"
+              className={styles.checkboxSignIn}
+            />
           </Form.Group>
         </Form>
       </div>
       <div className={styles.squareContainer}>
         <img src={logo} alt="Eventy Logo" className={styles.squareImage} />
-        <span className={styles.squareContainerDontHave}>Don't have an account?</span>
-        <Button variant="primary" type="submit" className={styles.submitSignInSignUp} onClick={handleSignUp}>
+        <span className={styles.squareContainerDontHave}>
+          Don't have an account?
+        </span>
+        <Button
+          variant="primary"
+          type="button"
+          className={styles.submitSignInSignUp}
+          onClick={handleSignUp}
+        >
           Sign up
         </Button>
       </div>
